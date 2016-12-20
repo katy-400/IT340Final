5d485a9e0327ed6d169ea4c9f54ae30e7397278d
katy-400 <katy.nyby@gmail.com>
2016-12-19 20:36:34 -0500

    Add files via upload

--- katt.sh
+++ katt.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-FILE="$1"
+FILE="$1" 
 DAYS="$2"
 CURDATE=$(date '+%b %d')
 PREVDATE=$(date -d $DAYS+' days ago' '+%b %d')
 
-#echo 'first arg: ' + $FILE + " second arg: " + $DAYS 
+#echo ' arg1: ' + $FILE + ' arg1: ' + $DAYS 
 #echo $CURDATE + $PREVDATE
 #curdate - days, curdate
 
-ERRORCNT=$(awk "/$PREVDATE/,/$CURDATE/" $FILE | grep -i -c 'error')
-echo 'Number of ERRORS for ' $FILE ' since ' $(date -d $DAYS+' days ago' '+%b %d') ': ' $ERRORCNT >> /var/log/katt.log
+COUNT=$(awk "/$PREVDATE/,/$CURDATE/" $FILE | grep -i -c 'error')
+echo 'ERRORS in ' $FILE ' from ' $(date -d $DAYS '+%b %d') ': ' $COUNT >> /var/log/katt.log
